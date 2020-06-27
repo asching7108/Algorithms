269c6982887c6710e61ac1215148c31de0e21a7c
asching7108 <asching7108@gmail.com>
2020-06-26 20:34:40 -0400

    Fixed warnings of assignment Percolation

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -12,148 +12,149 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-	private boolean[][] grid;
-	// represents connections of open sites including the virtual top
-	// and bottom sites
-	private WeightedQuickUnionUF sites;
-	// represents connections of open sites including only the virtual
-	// top site to prevent backwash
-	private WeightedQuickUnionUF sitesNoBackWash;
-	private int virtualTopSite;
-	private int virtualBottomSite;
-	private int numOpenSites;
-	private int N;
+    private boolean[][] grid;
+    // represents connections of open sites including the virtual top
+    // and bottom sites
+    private final WeightedQuickUnionUF sites;
+    // represents connections of open sites including only the virtual
+    // top site to prevent backwash
+    private final WeightedQuickUnionUF sitesNoBackWash;
+    private final int virtualTopSite;
+    private final int virtualBottomSite;
+    private final int n;
+    private int numOpenSites;
 
-	/**
-	 * Creates n-by-n grid, with all sites initially blocked.
-	 *
-	 * @param n the side length of the grid
-	 */
-	public Percolation(int n) {
-		if (n < 1) {
-			throw new IllegalArgumentException("n must be greater than 0");
-		}
-		grid = new boolean[n + 1][n + 1];
-		sites = new WeightedQuickUnionUF(n * n + 2);
-		sitesNoBackWash = new WeightedQuickUnionUF(n * n + 1);
-		virtualTopSite = 0;
-		virtualBottomSite = n * n + 1;
-		numOpenSites = 0;
-		N = n;
-	}
+    /**
+     * Creates n-by-n grid, with all sites initially blocked.
+     *
+     * @param n the side length of the grid
+     */
+    public Percolation(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must be greater than 0");
+        }
+        grid = new boolean[n + 1][n + 1];
+        sites = new WeightedQuickUnionUF(n * n + 2);
+        sitesNoBackWash = new WeightedQuickUnionUF(n * n + 1);
+        virtualTopSite = 0;
+        virtualBottomSite = n * n + 1;
+        numOpenSites = 0;
+        this.n = n;
+    }
 
-	//
+    //
 
-	/**
-	 * Opens the site of the given indices if it is not open already.
-	 *
-	 * @param row the row index
-	 * @param col the column index
-	 */
-	public void open(int row, int col) {
-		validate(row, col);
-		if (grid[row][col]) return;
+    /**
+     * Opens the site of the given indices if it is not open already.
+     *
+     * @param row the row index
+     * @param col the column index
+     */
+    public void open(int row, int col) {
+        validate(row, col);
+        if (grid[row][col]) return;
 
-		// opens the site
-		grid[row][col] = true;
-		numOpenSites++;
-		// connects both site unions to the virtual top site
-		if (row == 1) {
-			sites.union(serial(row, col), virtualTopSite);
-			sitesNoBackWash.union(serial(row, col), virtualTopSite);
-		}
-		// connects only the regular site union to the virtual bottom site
-		if (row == N) {
-			sites.union(serial(row, col), virtualBottomSite);
-		}
-		// connects both site unions to the site at top
-		if (row > 1 && isOpen(row - 1, col)) {
-			sites.union(serial(row, col), serial(row - 1, col));
-			sitesNoBackWash.union(serial(row, col), serial(row - 1, col));
-		}
-		// connects both site unions to the site at bottom
-		if (row < N && isOpen(row + 1, col)) {
-			sites.union(serial(row, col), serial(row + 1, col));
-			sitesNoBackWash.union(serial(row, col), serial(row + 1, col));
-		}
-		// connects both site unions to the site at left
-		if (col > 1 && isOpen(row, col - 1)) {
-			sites.union(serial(row, col), serial(row, col - 1));
-			sitesNoBackWash.union(serial(row, col), serial(row, col - 1));
-		}
-		// connects both site unions to the site at right
-		if (col < N && isOpen(row, col + 1)) {
-			sites.union(serial(row, col), serial(row, col + 1));
-			sitesNoBackWash.union(serial(row, col), serial(row, col + 1));
-		}
-	}
+        // opens the site
+        grid[row][col] = true;
+        numOpenSites++;
+        // connects both site unions to the virtual top site
+        if (row == 1) {
+            sites.union(serial(row, col), virtualTopSite);
+            sitesNoBackWash.union(serial(row, col), virtualTopSite);
+        }
+        // connects only the regular site union to the virtual bottom site
+        if (row == n) {
+            sites.union(serial(row, col), virtualBottomSite);
+        }
+        // connects both site unions to the site at top
+        if (row > 1 && isOpen(row - 1, col)) {
+            sites.union(serial(row, col), serial(row - 1, col));
+            sitesNoBackWash.union(serial(row, col), serial(row - 1, col));
+        }
+        // connects both site unions to the site at bottom
+        if (row < n && isOpen(row + 1, col)) {
+            sites.union(serial(row, col), serial(row + 1, col));
+            sitesNoBackWash.union(serial(row, col), serial(row + 1, col));
+        }
+        // connects both site unions to the site at left
+        if (col > 1 && isOpen(row, col - 1)) {
+            sites.union(serial(row, col), serial(row, col - 1));
+            sitesNoBackWash.union(serial(row, col), serial(row, col - 1));
+        }
+        // connects both site unions to the site at right
+        if (col < n && isOpen(row, col + 1)) {
+            sites.union(serial(row, col), serial(row, col + 1));
+            sitesNoBackWash.union(serial(row, col), serial(row, col + 1));
+        }
+    }
 
-	/**
-	 * Returns true / false if the site of the given indices is open.
-	 *
-	 * @param row the row index
-	 * @param col the column index
-	 * @return true if the site is open, otherwise returns false
-	 */
-	public boolean isOpen(int row, int col) {
-		validate(row, col);
-		return grid[row][col];
-	}
+    /**
+     * Returns true / false if the site of the given indices is open.
+     *
+     * @param row the row index
+     * @param col the column index
+     * @return true if the site is open, otherwise returns false
+     */
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return grid[row][col];
+    }
 
-	/**
-	 * Returns true / false if the site of the given indices is full.
-	 *
-	 * @param row the row index
-	 * @param col the column index
-	 * @return true if the site is full, otherwise returns false
-	 */
-	public boolean isFull(int row, int col) {
-		validate(row, col);
-		return sitesNoBackWash.connected(serial(row, col), virtualTopSite);
-	}
+    /**
+     * Returns true / false if the site of the given indices is full.
+     *
+     * @param row the row index
+     * @param col the column index
+     * @return true if the site is full, otherwise returns false
+     */
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        return sitesNoBackWash.find(serial(row, col)) ==
+            sitesNoBackWash.find(virtualTopSite);
+    }
 
-	/**
-	 * Returns the number of open sites.
-	 *
-	 * @return the number of open sites
-	 */
-	public int numberOfOpenSites() {
-		return numOpenSites;
-	}
+    /**
+     * Returns the number of open sites.
+     *
+     * @return the number of open sites
+     */
+    public int numberOfOpenSites() {
+        return numOpenSites;
+    }
 
-	/**
-	 * Returns true / false if the system percolates.
-	 *
-	 * @return true if the system percolates, otherwise returns false
-	 */
-	public boolean percolates() {
-		return sites.connected(virtualTopSite, virtualBottomSite);
-	}
+    /**
+     * Returns true / false if the system percolates.
+     *
+     * @return true if the system percolates, otherwise returns false
+     */
+    public boolean percolates() {
+        return sites.find(virtualTopSite) == sites.find(virtualBottomSite);
+    }
 
-	/**
-	 * Returns the 1D index of the given indices.
-	 *
-	 * @param row the row index
-	 * @param col the column index
-	 * @return the 1D index of the given indices
-	 */
-	private int serial(int row, int col) {
-		validate(row, col);
-		return (row - 1) * N + col;
-	}
+    /**
+     * Returns the 1D index of the given indices.
+     *
+     * @param row the row index
+     * @param col the column index
+     * @return the 1D index of the given indices
+     */
+    private int serial(int row, int col) {
+        validate(row, col);
+        return (row - 1) * n + col;
+    }
 
-	/**
-	 * Validates that the given indices are between 1 and N.
-	 *
-	 * @param row the row index
-	 * @param col the column index
-	 */
-	private void validate(int row, int col) {
-		if (row < 1 || row > N) {
-			throw new IndexOutOfBoundsException("row index out of bound");
-		}
-		if (col < 1 || col > N) {
-			throw new IndexOutOfBoundsException("column index out of bound");
-		}
-	}
+    /**
+     * Validates that the given indices are between 1 and n.
+     *
+     * @param row the row index
+     * @param col the column index
+     */
+    private void validate(int row, int col) {
+        if (row < 1 || row > n) {
+            throw new IllegalArgumentException("row index out of bound");
+        }
+        if (col < 1 || col > n) {
+            throw new IllegalArgumentException("column index out of bound");
+        }
+    }
 }
--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -9,84 +9,87 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-	private Percolation[] P;    // array of Percolation objects
-	private int[] thresholds;    // array of thresholds of Percolation objects
-	private int T;                // the number of trials
+    private static final double CONFIDENCE_95 = 1.96;
 
-	/**
-	 * Perform independent trials on an n-by-n grid.
-	 *
-	 * @param n      the side length of the grid
-	 * @param trials the number of trials
-	 */
-	public PercolationStats(int n, int trials) {
-		if (n < 1) {
-			throw new IllegalArgumentException("n must be greater than 0");
-		}
-		if (trials < 1) {
-			throw new IllegalArgumentException("trials must be greater than 0");
-		}
-		// initializes variables
-		T = trials;
-		P = new Percolation[T];
-		thresholds = new int[T];
+    private final double[] thresholds; // array of thresholds of Percolation objects
+    private final int trials;       // the number of trials
 
-		// performs experiments and record threshold
-		for (int i = 0; i < T; i++) {
-			P[i] = new Percolation(n);
-			while (!P[i].percolates()) {
-				int r = StdRandom.uniform(n * n);
-				while (P[i].isOpen(r / n + 1, r % n + 1)) {
-					r = StdRandom.uniform(n * n);
-				}
-				P[i].open(r / n + 1, r % n + 1);
-				thresholds[i]++;
-			}
-		}
-	}
+    /**
+     * Perform independent trials on an n-by-n grid.
+     *
+     * @param n      the side length of the grid
+     * @param trials the number of trials
+     */
+    public PercolationStats(int n, int trials) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must be greater than 0");
+        }
+        if (trials < 1) {
+            throw new IllegalArgumentException("trials must be greater than 0");
+        }
+        // initializes variables
+        thresholds = new double[trials];
+        this.trials = trials;
 
-	/**
-	 * Returns the sample mean of percolation threshold.
-	 *
-	 * @return the sample mean of percolation threshold
-	 */
-	public double mean() {
-		return StdStats.mean(thresholds);
-	}
+        // performs experiments and record threshold
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            while (!p.percolates()) {
+                int r = StdRandom.uniform(n * n);
+                while (p.isOpen(r / n + 1, r % n + 1)) {
+                    r = StdRandom.uniform(n * n);
+                }
+                p.open(r / n + 1, r % n + 1);
+                thresholds[i]++;
+            }
+            thresholds[i] /= (n * n);
+        }
+    }
 
-	/**
-	 * Returns the sample standard deviation of percolation threshold.
-	 *
-	 * @return the sample standard deviation of percolation threshold
-	 */
-	public double stddev() {
-		return StdStats.stddev(thresholds);
-	}
+    /**
+     * Returns the sample mean of percolation threshold.
+     *
+     * @return the sample mean of percolation threshold
+     */
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
 
-	/**
-	 * Returns the low endpoint of 95% confidence interval.
-	 *
-	 * @return the low endpoint of 95% confidence interval
-	 */
-	public double confidenceLo() {
-		return mean() - 1.96 * stddev() / Math.sqrt(T);
-	}
+    /**
+     * Returns the sample standard deviation of percolation threshold.
+     *
+     * @return the sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
 
-	/**
-	 * Returns the high endpoint of 95% confidence interval.
-	 *
-	 * @return the high endpoint of 95% confidence interval
-	 */
-	public double confidenceHi() {
-		return mean() + 1.96 * stddev() / Math.sqrt(T);
-	}
+    /**
+     * Returns the low endpoint of 95% confidence interval.
+     *
+     * @return the low endpoint of 95% confidence interval
+     */
+    public double confidenceLo() {
+        return mean() - CONFIDENCE_95 * stddev() / Math.sqrt(trials);
+    }
 
-	// test client
-	public static void main(String[] args) {
-		PercolationStats ps = new PercolationStats(20, 10);
-		System.out.println(ps.mean());
-		System.out.println(ps.stddev());
-		System.out.println(ps.confidenceLo());
-		System.out.println(ps.confidenceHi());
-	}
+    /**
+     * Returns the high endpoint of 95% confidence interval.
+     *
+     * @return the high endpoint of 95% confidence interval
+     */
+    public double confidenceHi() {
+        return mean() + CONFIDENCE_95 * stddev() / Math.sqrt(trials);
+    }
+
+    // test client
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, t);
+        String conf = "[" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]";
+        System.out.printf("%-23s %s %s%n", "mean", "=", ps.mean());
+        System.out.printf("%-23s %s %s%n", "stddev", "=", ps.stddev());
+        System.out.println("95% confidence interval = " + conf);
+    }
 }
